2f94fff0e719e8e3622384b4be1758c78c780c96
juliussarka-stack <julius.sarka@gmail.com>
2025-12-09 19:37:59 +0100

    Update process-offer-job.js

--- api/ai/process-offer-job.js
+++ api/ai/process-offer-job.js
@@ -1,1 +1,7 @@
-module.exports = require("./process-offer-job-handler.cjs");
+const applyCors = require("../lib/cors.cjs");
+
+module.exports = async (req, res) => {
+  if (applyCors(req, res)) return res.status(204).end();
+  const handler = require("./process-offer-job-handler.cjs"); // lazy-load
+  return handler(req, res);
+};
