decd9182745bff0c8057e7c7b7cf389291b6817d
juliussarka-stack <julius.sarka@gmail.com>
2025-12-09 22:25:38 +0100

    hotfix(4.3.10): fix work-tasks-database export to enhanced cjs

--- api/lib/work-tasks-database.cjs
+++ api/lib/work-tasks-database.cjs
@@ -1,1 +1,11 @@
-module.exports = require("./work-tasks-database.js");
+/**
+ * work-tasks-database.cjs
+ * Version: 7.0.4-patch-4.3.10
+ *
+ * This file is required by handlers as "./lib/work-tasks-database.cjs".
+ * The previous stub pointed to a non-existent .js file.
+ *
+ * Fix: re-export the existing CJS dataset in this folder.
+ */
+
+module.exports = require("./work-tasks-enhanced.cjs");
