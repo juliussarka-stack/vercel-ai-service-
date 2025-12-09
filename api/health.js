91c7f49c54355f5e7ce4dc280f75fe3926737326
juliussarka-stack <julius.sarka@gmail.com>
2025-12-09 19:35:52 +0100

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,2 +1,7 @@
-// Route file for Vercel discovery (CommonJS passthrough)
-module.exports = require("./health-handler.cjs");
+const applyCors = require("./lib/cors.cjs");
+const handler = require("./health-handler.cjs");
+
+module.exports = async (req, res) => {
+  if (applyCors(req, res)) return res.status(204).end();
+  return handler(req, res);
+};
