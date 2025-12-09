8e13586a86fd19dd37271e944c80478e36e4a10f
juliussarka-stack <julius.sarka@gmail.com>
2025-12-09 19:36:19 +0100

    Update test-cors.js

--- api/test-cors.js
+++ api/test-cors.js
@@ -1,1 +1,7 @@
-module.exports = require("./test-cors-handler.cjs");
+const applyCors = require("./lib/cors.cjs");
+const handler = require("./test-cors-handler.cjs");
+
+module.exports = async (req, res) => {
+  if (applyCors(req, res)) return res.status(204).end();
+  return handler(req, res);
+};
